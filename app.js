efb7cea55161972b4232518d62d8f41e03659cb1
Corentin Lemoine <corentinlemoine22@gmail.com>
2020-04-25 14:30:31 +0100

    Serial version

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const SerialPort = require('serialport');
+//const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort("/dev/ttyUSB0", { baudRate: 57600 });
+//const port = new SerialPort("/dev/ttyUSB0", { baudRate: 57600 });
 const Influx = require('influx');
+const { spawn } = require('child_process')
 
-const parser = new Readline();
-port.pipe(parser);
+const BME280 = require('bme280-sensor');
+const options = {
+    i2cBusNo: 1, // defaults to 1
+    i2cAddress: 0x76 // defaults to 0x77
+};
+
+const bme280 = new BME280(options);
 
 var fields = ["weight", "temperature", "humidity", "voltage"];
 
@@ -21,15 +27,15 @@ fields.forEach(field => {
     });
 });
 
-const influx = new Influx.InfluxDB({
-    host: 'localhost',
-    database: 'hives',
-    schema: schemas
-})
+const influx = new Influx.InfluxDB('http://admin:extra@127.0.0.1:8086/hives');
+influx.schemas = schemas;
 
-
+const python = spawn('python', ['script1.py']);
+const parser = new Readline();
+process.pipe(python.stdout);
 
 parser.on('data', function (line) {
+    console.log(line);
     try {
         data = JSON.parse(line);
         console.log(data)
@@ -49,3 +55,50 @@ parser.on('data', function (line) {
     }
 
 });
+
+const readSensorData = () => {
+    var i = 0;
+
+    var temp = [];
+    var pres = [];
+    var measure = setInterval(() => {
+        if (i < 5) {
+            bme280.readSensorData()
+                .then((data) => {
+                    temp.push(data.temperature_C);
+                    pres.push(data.pressure_hPa);
+                    console.log(`BME280 reading: ${JSON.stringify(data)}`)
+                    i += 1;
+                })
+                .catch((err) => {
+                    console.log(`BME280 read error: ${err}`);
+                });
+        } else {
+            clearInterval(measure);
+            var points = [];
+            points.push({
+                measurement: "temperature",
+                tags: { hive: "sensor_1" },
+                fields: { value: temp.sort()[2] }
+            });
+            points.push({
+                measurement: "pressure",
+                tags: { hive: "sensor_1" },
+                fields: { value: pres.sort()[2] }
+            });
+            influx.writePoints(points);
+            setTimeout(readSensorData, 1000 * 60 * 10);
+            i = 0;
+        }
+    }, 5000);
+};
+
+// Initialize the BME280 sensor
+//
+bme280.init()
+    .then(() => {
+        console.log('BME280 initialization succeeded');
+        readSensorData();
+    })
+    .catch((err) => console.error(`BME280 initialization failed: ${err} `));
+
